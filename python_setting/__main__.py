5cd4ae713e523bdcf97cb76dedad5d35d4394584
tmk <morimoto.ke@gmail.com>
2024-03-27 21:20:26 +0900

    ✨ 割り算機能追加

--- python_setting/__main__.py
+++ python_setting/__main__.py
@@ -1,3 +1,23 @@
+# ! 割り算
+
+
+def divide(x: int | float, y: int | float) -> float | None:
+    """_summary_
+
+    Args:
+        x (int | float): _description_
+        y (int | float): _description_
+
+    Returns:
+        float | None: _description_
+    """
+    try:
+        result = x / y
+        return result
+    except ZeroDivisionError as e:
+        print(f"Error Occurred: {e}")
+
+
 def python_setting():
     print("hello world!")
 
